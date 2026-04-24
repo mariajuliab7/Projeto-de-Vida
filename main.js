e6646296328725c3b6726b52fd0a8dcab94ad302
mariajuliab7 <maria.juliane.bueno@escola.pr.gov.br>
2026-04-24 09:06:05 -0300

    gg

--- main.js
+++ main.js
@@ -16,8 +16,13 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2032-04-25T00:00:00");
+const tempoObjetivo2 = new Date("2032-05-25T00:00:00");
+const tempoObjetivo3 = new Date("2032-06-25T00:00:00");
+const tempoObjetivo4 = new Date("2032-07-25T00:00:00");
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo){
    let tempoAtual = new Date();
