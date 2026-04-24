7d1733fd8c7885ec19b4d67522d8956ad540615b
mariajuliab7 <maria.juliane.bueno@escola.pr.gov.br>
2026-04-24 09:26:27 -0300

    gg

--- main.js
+++ main.js
@@ -20,9 +20,7 @@ const tempoObjetivo2 = new Date("2032-05-25T00:00:00");
 const tempoObjetivo3 = new Date("2032-06-25T00:00:00");
 const tempoObjetivo4 = new Date("2032-07-25T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
    let tempoAtual = new Date();
@@ -35,7 +33,23 @@ function calculaTempo(tempoObjetivo){
    segundos %= 60;
    minutos %= 60;
    horas %= 24;
-
-   return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+   if(tempoFinal > 0){
+       return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++);{
+    contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
