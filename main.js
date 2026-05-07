d4230f72fd85dbe0f3a06f2db06012d1c53fc0a2
mariajuliab7 <maria.juliane.bueno@escola.pr.gov.br>
2026-05-07 13:23:51 -0300

    gg

--- main.js
+++ main.js
@@ -36,14 +36,14 @@ function calculaTempo(tempoObjetivo){
    if(tempoFinal > 0){
        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 
 }
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
+        contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 
